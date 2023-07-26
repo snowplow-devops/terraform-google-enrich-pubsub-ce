45d190900ae95930da8ece2cd5d8f39860e36e2b
jbeemster <joshua.a.beemster@gmail.com>
2023-07-26 10:44:03 +1000

    Leverage common service module (closes #13)

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "manager_id" {
-  value       = google_compute_region_instance_group_manager.grp.id
+  value       = module.service.manager_id
   description = "Identifier for the instance group manager"
 }
 
 output "manager_self_link" {
-  value       = google_compute_region_instance_group_manager.grp.self_link
+  value       = module.service.manager_self_link
   description = "The URL for the instance group manager"
 }
 
 output "instance_group_url" {
-  value       = google_compute_region_instance_group_manager.grp.instance_group
+  value       = module.service.instance_group_url
   description = "The full URL of the instance group created by the manager"
 }
